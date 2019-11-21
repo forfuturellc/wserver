067d2c27a13cb8b4e458b52999c70c2bfdf129e0
GochoMugo <mugo@forfuture.co.ke>
2019-11-22 01:09:06 +0300

    src: Use native async/await

--- dist/lib/server.js
+++ dist/lib/server.js
@@ -4,13 +4,21 @@
  *
  * WebSocket server.
  */
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 // built-in modules
 const assert = require("assert");
 const EventEmitter = require("events");
 const url = require("url");
 // installed modules
-const asyncawait_1 = require("asyncawait");
 const Debug = require("debug");
 const WebSocket = require("ws");
 // own modules
@@ -30,25 +38,23 @@ class Server extends EventEmitter {
             pingInterval: 60 * 1000,
             ignoreConnReset: true,
         }, options);
-        this.close = asyncawait_1.async(this.close); // TODO/types:any
-        this.notifyAll = asyncawait_1.async(this.notifyAll); // TODO/types:any
-        this.registerSocket = asyncawait_1.async(this.registerSocket); // TODO/types:any
-        this.handleRequest = asyncawait_1.async(this.handleRequest).bind(this); // TODO/types:any
         this.wss = new WebSocket.Server({ server, path: this.options.path });
         this.wss.on("connection", this.registerSocket.bind(this));
         this.pingTimeout = setInterval(this.pingSockets.bind(this), this.options.pingInterval);
     }
     close() {
-        assert.equal(this.closing, false, "WebSocket-Server already closing");
-        this.closing = true;
-        clearInterval(this.pingTimeout);
-        for (const socket of this.sockets) {
-            asyncawait_1.await(socket.close(constants.WEBSOCKET_CLOSE_CODES.TRY_AGAIN_LATER));
-        }
-        asyncawait_1.await(new Promise((a, b) => {
-            this.wss.close((e) => e ? b(e) : a());
-        }));
-        return;
+        return __awaiter(this, void 0, void 0, function* () {
+            assert.equal(this.closing, false, "WebSocket-Server already closing");
+            this.closing = true;
+            clearInterval(this.pingTimeout);
+            for (const socket of this.sockets) {
+                yield (socket.close(constants.WEBSOCKET_CLOSE_CODES.TRY_AGAIN_LATER));
+            }
+            yield (new Promise((a, b) => {
+                this.wss.close((e) => e ? b(e) : a());
+            }));
+            return;
+        });
     }
     pingSockets() {
         const sieve = [];
@@ -64,43 +70,49 @@ class Server extends EventEmitter {
         this.sockets = sieve;
     }
     notifyAll(event, payload) {
-        asyncawait_1.await(this.sockets.map((socket) => socket.notify(event, payload)));
-        return;
+        return __awaiter(this, void 0, void 0, function* () {
+            yield (this.sockets.map((socket) => socket.notify(event, payload)));
+            return;
+        });
     }
     registerSocket(ws, req) {
-        debug("handling new socket");
-        const socket = new socket_1.default(ws, {
-            ignoreConnReset: this.options.ignoreConnReset,
+        return __awaiter(this, void 0, void 0, function* () {
+            debug("handling new socket");
+            const socket = new socket_1.default(ws, {
+                ignoreConnReset: this.options.ignoreConnReset,
+            });
+            if (this.closing) {
+                return socket.close(new Error("Server is shutting down"));
+            }
+            if (this.options.authenticateSocket) {
+                req.query = url.parse(req.url, true).query;
+                try {
+                    socket.profile = yield (this.options.authenticateSocket(req));
+                }
+                catch (error) {
+                    return socket.close(error);
+                }
+            }
+            this.emit("socket", socket);
+            this.sockets.push(socket);
+            socket.on("request", this.handleRequest.bind(this));
         });
-        if (this.closing) {
-            return socket.close(new Error("Server is shutting down"));
-        }
-        if (this.options.authenticateSocket) {
-            req.query = url.parse(req.url, true).query;
-            try {
-                socket.profile = asyncawait_1.await(this.options.authenticateSocket(req));
-            }
-            catch (error) {
-                return socket.close(error);
-            }
-        }
-        this.emit("socket", socket);
-        this.sockets.push(socket);
-        socket.on("request", this.handleRequest);
     }
     handleRequest(request, socket) {
-        debug("handling message from socket: %s", request.action);
-        if (!this.options.handleRequest) {
-            return socket.rejectRequest(request, new Error("NotImplemented"));
-        }
-        let result;
-        try {
-            result = asyncawait_1.await(this.options.handleRequest(request, socket));
-        }
-        catch (error) {
-            return socket.rejectRequest(request, error);
-        }
-        return socket.acceptRequest(request, result);
+        return __awaiter(this, void 0, void 0, function* () {
+            debug("handling message from socket: %s", request.action);
+            if (!this.options.handleRequest) {
+                return socket.rejectRequest(request, new Error("NotImplemented"));
+            }
+            let result;
+            try {
+                result = yield (this.options.handleRequest(request, socket));
+            }
+            catch (error) {
+                return socket.rejectRequest(request, error);
+            }
+            return socket.acceptRequest(request, result);
+        });
     }
 }
 exports.default = Server;
