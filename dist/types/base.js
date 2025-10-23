930fb6e6b55f87e8b9f8f357814db1de13bcbcd1
GochoMugo <mugo@forfuture.co.ke>
2025-10-23 19:21:47 +0300

    chore: dont commit build files

--- dist/types/base.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Copyright (c) 2017 Forfuture, LLC (https://forfuture.tech)
- *
- * Base types.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=base.js.map
--- dist/types/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-/**
- * Copyright (c) 2017 Forfuture, LLC (https://forfuture.tech)
- *
- * Server component.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=server.js.map
